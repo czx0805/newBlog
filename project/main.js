91d53f2392f4929d6a56a8278e28265bf53ba0cc
ChenJon <Jon@ChenJondeMacBook-Pro.local>
2016-08-22 23:09:49 +0800

    add

---
+++ project/main.js
@@ -0,0 +1,18 @@
+function loadPage(type){
+	location.href = "infoList.html?"+type;
+}
+$(function(){
+	setTimeout(function(){
+		$("header").css({"transform":"scale(1)","transition": "1s"});	
+		$(".tag").css({transform: "translateY(0)"});
+	},0);
+	$(".tag").eq(0).click(function(){
+		loadPage("work");
+	});
+	$(".tag").eq(1).click(function(){
+		loadPage("blog");
+	});
+	$(".tag").eq(2).click(function(){
+		loadPage("about");
+	});
+});
